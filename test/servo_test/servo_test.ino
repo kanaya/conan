7de4ec677ee2de70d24b7a0933549f1a96cb5654
Ichi Kanaya <kanaya@pineapple.cc>
2024-02-15 08:31:19 +0900

    test.

---
+++ test/servo_test/servo_test.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo myservo;
+const int SV_PIN = 6;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  myservo.attach(SV_PIN, 500, 2400);
+}
+
+void loop() {
+  myservo.write(180);
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  myservo.write(90);
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+}
